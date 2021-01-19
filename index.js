1dcfe59f95fdaf7c6cab42f5faccab75af93b9da
yyhun1200 <yyhun1200@gmail.com>
2021-01-19 22:45:57 +0900

    add register

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// application/json
+app.use(express.json());
+
 mongoose
   .connect(
     "mongodb+srv://yyhun1200:yy1200@boiler-plate.dyx0k.mongodb.net/boiler-plate?retryWrites=true&w=majority",
     {
-      userNewUrlParser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false,
@@ -16,4 +24,18 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (reg, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 clinet에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
