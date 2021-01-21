47b87100a00338904acd39118dc9e66630d8b212
yyhun1200 <yyhun1200@gmail.com>
2021-01-21 22:52:18 +0900

    add generateToken

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 const mongoose = require("mongoose");
@@ -11,7 +11,7 @@ const { User } = require("./models/User");
 app.use(express.urlencoded({ extended: true }));
 // application/json
 app.use(express.json());
-
+app.use(cookieParser());
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -32,6 +32,35 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호가 같은지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      // 비밀번호까지  맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        // token을 저장한다. 어디에? 쿠키 or 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
