0c6d001af1fc7439efb0a29ccf73e75adb6cb9bb
yyhun1200 <yyhun1200@gmail.com>
2021-01-22 22:40:38 +0900

    add Auth

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const config = require("./config/key");
 
 const mongoose = require("mongoose");
 const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 
 // application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +25,7 @@ mongoose
 
 app.get("/", (reg, res) => res.send("Hello World!"));
 
-app.post("/register", (req, res) => {
+app.post("api/users/register", (req, res) => {
   // 회원 가입 할때 필요한 정보들을 clinet에서 가져오면
   // 그것들을 데이터베이스에 넣어준다
   const user = new User(req.body);
@@ -36,7 +37,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -64,6 +65,21 @@ app.post("/login", (req, res) => {
     });
   });
 });
+
+app.get("api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 이야기는 Authentication 이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
