424f1b775695bddd08873660e5f618fbc57bb9e4
yyhun1200 <yyhun1200@gmail.com>
2021-01-26 23:38:26 +0900

    set react-route

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
