da735e06fd104dbcc97ccb27d77520411ca9bf78
yyhun1200 <yyhun1200@gmail.com>
2021-01-30 17:01:51 +0900

    add login, register

--- client/src/components/views/LoginPage/LoginPage.jsx
+++ client/src/components/views/LoginPage/LoginPage.jsx
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
 
-const LoginPage = (props) => <div>LoginPage</div>;
+const LoginPage = (props) => {
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const body = {
+      email: Email,
+      password: Password,
+    };
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        props.history.push("/");
+      } else {
+        alert("Error");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <label htmlFor="">Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label htmlFor="">Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
--- client/src/components/views/RegisterPage/RegisterPage.jsx
+++ client/src/components/views/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
 
-const RegisterPage = (props) => <div>RegisterPage</div>;
+const RegisterPage = (props) => {
+  const dispatch = useDispatch();
+  const [Email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (Password !== ConfirmPassword) {
+      return alert("바말번호와 비밀번화 확인은 같아야 합니다.");
+    }
+    const body = {
+      email: Email,
+      name: Name,
+      password: Password,
+    };
+
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to sign up");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <label htmlFor="">Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label htmlFor="">Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        <label htmlFor="">Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label htmlFor="">Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+        <br />
+        <button>회원가입</button>
+      </form>
+    </div>
+  );
+};
 
 export default RegisterPage;
